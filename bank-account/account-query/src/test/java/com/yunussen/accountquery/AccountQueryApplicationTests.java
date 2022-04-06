fc180174c0c395372d62bb20b1f1c3969d5758c3
YUNUS <yunussen@ycp.com.tr>
2022-04-06 15:46:02 +0300

    add: Project folder infrastructure and dependencies were created.

---
+++ bank-account/account-query/src/test/java/com/yunussen/accountquery/AccountQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunussen.accountquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
