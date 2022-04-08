09274c5395e923ed293362bc81623bfa6f1c21eb
YUNUS <yunussen@ycp.com.tr>
2022-04-08 11:55:19 +0300

    add: DepositFundsController

---
+++ bank-account/account-cmd/src/main/java/com/yunussen/accountcmd/api/controllers/DepositFundsController.java
@@ -0,0 +1,41 @@
+package com.yunussen.accountcmd.api.controllers;
+
+import com.yunussen.accountcmd.api.commands.DepositFundsCommand;
+import com.yunussen.accountcommon.dto.BaseResponse;
+import com.yunussen.cqrscore.exception.AggregateNotFoundException;
+import com.yunussen.cqrscore.infrasstructure.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(path = "/api/v1/depositFunds")
+public class DepositFundsController {
+    private final Logger logger = Logger.getLogger(DepositFundsController.class.getName());
+
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> depositFunds(@PathVariable(value = "id") String id,
+                                                     @RequestBody DepositFundsCommand command) {
+        try {
+            command.setId(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<>(new BaseResponse("Deposit funds request completed successfully!"), HttpStatus.OK);
+        } catch (IllegalStateException | AggregateNotFoundException e) {
+            logger.log(Level.WARNING, MessageFormat.format("Client made a bad request - {0}.", e.toString()));
+            return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            var safeErrorMessage = MessageFormat.format("Error while processing request to deposit funds to bank account with id - {0}.", id);
+            logger.log(Level.SEVERE, safeErrorMessage, e);
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
